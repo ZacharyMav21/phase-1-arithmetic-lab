2288e7d6690db93aa8b49e8bcd9f4d90fbc21f66
Zachary <zepm0122.zn@gmail.com>
2024-06-25 13:36:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1*num2
+let random = Math.ceil(Math.random() * 100)
+let num3 = 40
+let num4 = 10
+let mod = num3 / num4
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
